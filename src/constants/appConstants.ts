5b62e94ebb5663499c6bde7a36174f1765822818
Diego Caicedo <dex.220488@live.com>
2024-10-08 18:59:28 -0600

    constants changed

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = "Vocabulary";
-export const DATA_IMAGE_SRC = "/assets/images/data/";
-export const GLOBAL_IMAGE_SRC = "/assets/images/";
+export const DATA_IMAGE_SRC = "https://dex220488.github.io/LearnEnglishVocabulary/assets/images/data/";
+export const GLOBAL_IMAGE_SRC = "https://dex220488.github.io/LearnEnglishVocabulary/assets/images/";
 
 export enum STATUS_ENUM {
   SUCCESS,
