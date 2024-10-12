c8211158df9485ae621d388635dcde9491b0d82a
Diego Caicedo <dex.220488@live.com>
2024-10-12 06:36:11 -0600

    added meaning

--- src/components/ImageSlider/components/WordImage/WordImage.styles.ts
+++ src/components/ImageSlider/components/WordImage/WordImage.styles.ts
@@ -10,7 +10,21 @@ export const StyledImage = styled("img")(({ theme }) => ({
 
 export const StyledImageContainer = styled("div")(({ theme }) => ({
   display: "flex",
+  flexDirection: "column",
+  gap: "6px",
   justifyContent: "center",
   alignItems: "center",
   width: "100%",
+  cursor: "pointer",
+}));
+
+export const StyledInfoContainer = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "8px",
+  color: "white",
+  backgroundColor: "rgba(33, 150, 243, 0.5)",
+  borderRadius: "8px",
+  padding: "10px 14px",
+  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
 }));
--- src/components/ImageSlider/components/WordImage/WordImage.tsx
+++ src/components/ImageSlider/components/WordImage/WordImage.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 import { Word } from "../../../../assets/data/data";
 import { DATA_IMAGE_SRC } from "../../../../constants";
-import { StyledImage, StyledImageContainer } from "./WordImage.styles";
+import { StyledImage, StyledImageContainer, StyledInfoContainer } from "./WordImage.styles";
+import InfoIcon from "@mui/icons-material/Info";
+import { Typography } from "@mui/material";
 
 type ImageProps = {
   word: Word;
 };
 
 export const WordImage: React.FC<ImageProps> = ({ word }) => {
-  const { id, value } = word;
+  const { id, value, meaning } = word;
   const imageSrc = `${DATA_IMAGE_SRC}${value.toLowerCase()}.jpeg`;
+  const [showInfo, setShowInfo] = React.useState(true);
 
   return (
     <StyledImageContainer key={id}>
-      <StyledImage src={imageSrc} alt={value} />
+      <StyledImage src={imageSrc} alt={value} onClick={() => setShowInfo(!showInfo)} />
+      {showInfo && (
+        <StyledInfoContainer>
+          <InfoIcon />
+          <Typography variant='body1' component='body'>
+            {meaning}
+          </Typography>
+        </StyledInfoContainer>
+      )}
     </StyledImageContainer>
   );
 };
